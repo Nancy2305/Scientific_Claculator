f3a8efb3645e1f0621e31a669ad0bffc7b227a5d
Nancy2305 <155437469+Nancy2305@users.noreply.github.com>
2024-03-24 19:46:34 +0000

    implemented few some scintific function

--- src/script.js
+++ src/script.js
@@ -6,39 +6,48 @@ document.addEventListener("DOMContentLoaded",function(){
 
     function evaluateResult(){
         console.log('currentValue: ',currentValue)
-        const convertedValue = currentValue
+        const convertedValue = currentValue.toString()
             .replace("×","*")
             .replace("÷","/")
             .replace("%","*0.01")
-            .replace('sin','Math.sin')
-            replace('cos','Math.cos');
-
-
+            .replace('sin','Math.sin') 
+            .replace('cos','Math.cos')
+            .replace('ln','Math.log')
+            .replace('π','Math.PI')
+            .replace('log','Math.log10')
+            .replace('e','Math.E')
+            .replace('tan','Math.tan')
+            .replace('√','Math.sqrt'); 
         console.log('convertedValue:',convertedValue)
         const result = eval(convertedValue);
         currentValue = result.toString();
         display.value = currentValue;
     }
-
-    for (let i = 0; i<buttons.length; i++){
+    
+   
+    
+    for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
-        button.addEventListener('click', function(){
+        button.addEventListener('click', function() {
             const value = button.innerText;
-
-            if(value =="AC"){
-                currentValue = "";
+    
+            try {
+                if (value === "AC") {
+                    currentValue = "";
+                    display.value = currentValue;
+                } else if (value === "=") {
+                    evaluateResult();
+                } else {
+                    currentValue += value;
+                    console.log('currentValue:', currentValue);
+                    display.value = currentValue;
+                }
+            } catch (error) {
+                console.error('An error occurred:', error);
+                currentValue = 'ERROR';
                 display.value = currentValue;
             }
-            else if(value == "="){
-                evaluateResult();
-            }
-            else{
-                currentValue += value;
-                console.log('currentValue:', currentValue);
-                display.value = currentValue;
-            }
-            
-
-        })
+        });
     }
-}); 
+    
+

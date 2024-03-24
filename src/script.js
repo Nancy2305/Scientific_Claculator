59cc0e4261cbb2b33324826521ba023b5d81d58b
Nancy2305 <155437469+Nancy2305@users.noreply.github.com>
2024-03-24 19:51:58 +0000

    corrected this

--- src/script.js
+++ src/script.js
@@ -1,36 +1,34 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function() {
     const display = document.getElementById('calc-display');
-    const buttons = document.getElementsByClassName('btn'); // Corrected variable name
-    
+    const buttons = document.getElementsByClassName('btn');
+
     let currentValue = "";
 
-    function evaluateResult(){
-        console.log('currentValue: ',currentValue)
+    function evaluateResult() {
+        console.log('currentValue: ', currentValue)
         const convertedValue = currentValue.toString()
-            .replace("×","*")
-            .replace("÷","/")
-            .replace("%","*0.01")
-            .replace('sin','Math.sin') 
-            .replace('cos','Math.cos')
-            .replace('ln','Math.log')
-            .replace('π','Math.PI')
-            .replace('log','Math.log10')
-            .replace('e','Math.E')
-            .replace('tan','Math.tan')
-            .replace('√','Math.sqrt'); 
-        console.log('convertedValue:',convertedValue)
+            .replace("×", "*")
+            .replace("÷", "/")
+            .replace("%", "*0.01")
+            .replace('sin', 'Math.sin')
+            .replace('cos', 'Math.cos')
+            .replace('ln', 'Math.log')
+            .replace('π', 'Math.PI')
+            .replace('log', 'Math.log10')
+            .replace('e', 'Math.E')
+            .replace('tan', 'Math.tan')
+            .replace('√', 'Math.sqrt');
+        console.log('convertedValue:', convertedValue)
         const result = eval(convertedValue);
         currentValue = result.toString();
         display.value = currentValue;
     }
-    
-   
-    
+
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
         button.addEventListener('click', function() {
             const value = button.innerText;
-    
+
             try {
                 if (value === "AC") {
                     currentValue = "";
@@ -49,5 +47,4 @@ document.addEventListener("DOMContentLoaded",function(){
             }
         });
     }
-    
-
+});
